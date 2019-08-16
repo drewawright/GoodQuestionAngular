87436537fb366cf62654e8d46d924dd84b8872fb
Kyle Davison <kyle.w.davison@gmail.com>
2019-08-16 14:45:00 -0400

    Good code here don't ask what X is

--- src/app/components/analyze-user/analyze-user.component.ts
+++ src/app/components/analyze-user/analyze-user.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SongService } from 'src/app/services/song.service';
 import { PlaylistService } from 'src/app/services/playlist.service';
 import { Playlist } from 'src/app/models/playlist';
-import { AppAuthService } from 'src/app/services/app-auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-analyze-user',
@@ -13,14 +13,36 @@ import { AppAuthService } from 'src/app/services/app-auth.service';
 export class AnalyzeUserComponent implements OnInit {
 
   playlistArray: Playlist[];
-  constructor(private _songService: SongService, private _playlistService: PlaylistService, private _appAuthService: AppAuthService) { }
+  constructor(private _songService: SongService, private _playlistService: PlaylistService, private _router: Router) { }
 
   ngOnInit() {
     this._playlistService.getPlaylistsSpotify().subscribe(res => console.log(res));
-    this._songService.getAllUserSongs().subscribe(res => console.log(res));
+    this.scheduleGetAllUserSongs();
+
+    //this._songService.getAllUserSongs().subscribe(res => console.log(res));
     //this._playlistService.getPlaylists().subscribe((res: Playlist[]) => console.log(res));
     //this._playlistService.getPlaylists().subscribe((res: Playlist[]) => res.forEach( (playlist) => { this._songService.getSongsInPlaylist(playlist.PlaylistId).subscribe(res => console.log(res))}));
     //this.playlistArray.forEach( (playlist) => {this._songService.getSongsInPlaylist(playlist.PlaylistId).subscribe(res => console.log(res))});
   }
 
+  resolveAfterYSeconds(x,y) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(x);
+      }, y);
+    });
+  }
+
+  scheduleGetAllUserSongs() {
+    this.resolveAfterYSeconds(20,2000).then(value => {
+      this._songService.getAllUserSongs().subscribe(res => console.log(res));
+      this.scheduleRoute();
+    });
+  }
+
+  scheduleRoute() {
+    this.resolveAfterYSeconds(20,30000).then(value => {
+      this._router.navigate(['playlist']);
+    });
+  }
 }
