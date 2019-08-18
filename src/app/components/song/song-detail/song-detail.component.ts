f6f4186ebdef649bc878d286274acba540bd3b16
jdorhauer <jdorhauer@yahoo.com>
2019-08-18 16:55:29 -0400

    Song details now loading on page

--- src/app/components/song/song-detail/song-detail.component.ts
+++ src/app/components/song/song-detail/song-detail.component.ts
@@ -11,38 +11,98 @@ import { Song } from 'src/app/models/song';
 export class SongDetailComponent implements OnInit {
 
   song: Song;
-  songUrl: string;
 
-  public chartType: string = "radar";
+  userData: number[] = [];
 
-  public chartDataSets: Array<any> = [
-    { data: [this.song.Acousticness, this.song.Danceability, this.song.Energy, this.song.Instrumentalness, this.song.Liveness, this.song.Speechiness, this.song.Valence], label: "Measure for Measure" }
+  public chartType: string = 'radar';
+
+  public chartDatasets: Array<any> = [
+    { data: this.userData
+      , label: 'Song Audio Data' },
   ];
 
-  public chartLabels: Array<any> = [ 'acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness', 'valence'];
+  public chartLabels: Array<any> = ['Danceabiltiy', 'Energy', 'Speechiness', 'Acousticness', 'Instrumentalness', 'Liveness', 'Valence'];
 
   public chartColors: Array<any> = [
     {
-      backgroundColor: 'rgba(105, 0, 132, .2)',
+      backgroundColor: 'rgba(143, 87, 225, .2)',
       borderColor: 'rgba(200, 99, 132, .7)',
       borderWidth: 2,
-    }
+    },
   ];
 
   public chartOptions: any = {
-    responsive: true
+    responsive: true,
+    scale:{
+      ticks: {
+        display: false
+      }
+    }
+  };
+
+  updateDataset() {
+    const chartData: number[] = this.userData;
+    this.chartDatasets = [
+      { data: chartData }
+    ]
   }
 
 
-
-  constructor(private _songService: SongService, private _activatedRoute: ActivatedRoute) { }
+  constructor(private _activatedRoute: ActivatedRoute, private _songService: SongService) { }
 
   ngOnInit() {
     this._activatedRoute.paramMap.subscribe(routeData => {
-      this._songService.getSongById(routeData.get('id')).subscribe((singleSong: Song) => {
-        this.song = singleSong; this.songUrl = singleSong.SongId;
+      this._songService.getSongById(routeData.get('id')).subscribe((pulledSong: Song) => {
+        this.song = pulledSong;
       });
     });
+    
+    this._activatedRoute.paramMap.subscribe(routeData => {
+      this._songService.getSongById(routeData.get('id')).subscribe((songResult: Song) => {
+        this.userData.push(songResult.Danceability, songResult.Energy, songResult.Speechiness, songResult.Acousticness, songResult.Instrumentalness, songResult.Liveness, songResult.Valence);
+      });
+    });
+
+    setTimeout(() => {
+      this.updateDataset()
+    }, 1000);
   }
 
 }
+
+
+//   songUrl: string;
+
+//   public chartType: string = "radar";
+
+//   public chartDataSets: Array<any> = [
+//     { data: [this.song.Acousticness, this.song.Danceability, this.song.Energy, this.song.Instrumentalness, this.song.Liveness, this.song.Speechiness, this.song.Valence], label: "Measure for Measure" }
+//   ];
+
+//   public chartLabels: Array<any> = [ 'acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness', 'valence'];
+
+//   public chartColors: Array<any> = [
+//     {
+//       backgroundColor: 'rgba(105, 0, 132, .2)',
+//       borderColor: 'rgba(200, 99, 132, .7)',
+//       borderWidth: 2,
+//     }
+//   ];
+
+//   public chartOptions: any = {
+//     responsive: true
+//   }
+
+
+
+//   constructor(private _songService: SongService, private _activatedRoute: ActivatedRoute) { }
+
+//   ngOnInit() {
+//     this._activatedRoute.paramMap.subscribe(routeData => {
+//       this._songService.getSongById(routeData.get('id')).subscribe((singleSong: Song) => {
+//         this.song = singleSong; this.songUrl = singleSong.SongId;
+//       });
+//     });
+//   }
+
+// }
